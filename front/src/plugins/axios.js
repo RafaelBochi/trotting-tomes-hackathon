adc47e3d3d4ee6fb1df930bf724ffa0a0a22aa24
faelbochi <106544702+RafaelBochi@users.noreply.github.com>
2023-10-25 14:52:15 -0300

    Update axios.js

--- front/src/plugins/axios.js
+++ front/src/plugins/axios.js
@@ -1,4 +1,4 @@
 import axios from 'axios';
 
 axios.defaults.baseURL = 'http://localhost:8000/'
-// axios.defaults.baseURL = 'https://deploy-hackathon-back-7hee-dev.fl0.io/'
+// axios.defaults.baseURL = 'https://trottingtomes.2.ie-1.fl0.io/'
